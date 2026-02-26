60149c7f77c03c9d1b7d40b54b687ae3870e05ec
Maik Lorenz <maik.lorenz@pm.me>
2026-02-26 13:51:12 +0100

    add aesdsocket-start-stop.sh

---
+++ server/aesdsocket-start-stop.sh
@@ -0,0 +1,25 @@
+EXECUTABLE=/usr/bin/aesdsocket
+EXECUTABLE=$(pwd)/aesdsocket
+PIDFILE=/var/run/aesdsocket.pid
+
+case "$1" in 
+    start)
+        echo "Starting aesdsocket..."
+        start-stop-daemon \
+            --start \
+            --background \
+            --make-pidfile \
+            --pidfile $PIDFILE \
+            --exec $EXECUTABLE -- -d
+        ;;
+    stop)
+        echo "Stopping aesdsocket..."
+        start-stop-daemon \
+            --stop \
+            --pidfile $PIDFILE \
+            --signal TERM
+        ;;
+    *)
+        echo "Usage: $0 {start|stop}"
+    exit 1
+esac
